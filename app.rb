70f4139d4bc295ab5dd68847deb0e3b92e33ffbb
Precious Betine <preciousbetine@gmail.com>
2023-05-18 15:40:03 +0100

    Create app structure

---
+++ app.rb
@@ -0,0 +1,31 @@
+require './classes/book'
+
+class App
+  def initialize
+    @books = []
+  end
+
+  def list_all_books
+    puts 'All available books:'
+  end
+
+  def list_all_people
+
+  end
+
+  def create_person
+
+  end
+
+  def create_book
+    
+  end
+
+  def create_rental
+
+  end
+
+  def list_person_rentals(person_id)
+
+  end
+end
---
+++ main.rb
@@ -0,0 +1,37 @@
+require './app.rb'
+
+def show_menu
+  puts 'Please choose an option by entering a number:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List all rentals for a given person id'
+  puts '7. Exit'
+  print 'Your input: '
+end
+
+def main
+  app = App.new
+  loop do
+    show_menu()
+    input = gets.chomp
+    case input.to_i
+    when 1
+    when 2
+    when 3
+    when 4
+    when 5
+    when 6
+    when 7
+      puts 'Goodbye!'
+      break
+    else
+      puts
+      puts 'Please enter a valid option!'
+    end
+  end
+end
+
+main()
