14deb4aec0d2019169a3aca697bcf74a84f97a4c
Walid Kabboussa <walidkabou@gmail.com>
2023-05-31 17:07:11 +0100

    Fix teacher test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,10 +3,15 @@ require_relative '../classes/teacher.rb'
 describe 'Teacher' do
   context 'teacher' do 
     it 'should intitalize a teacher' do 
-        teacher = Teacher.new(30, name='Unknown', specialization:'Physics')
+        teacher = Teacher.new(30, 'Physics', name='Unknown')
         expect(teacher.name).to eq 'Unknown'
         expect(teacher.age).to eq 30 
-        expect(teacher.specialization).to eq ' Physics' 
+        expect(teacher.specialization).to eql 'Physics' 
+        expect(teacher.parent_permission).to be true
+    end
+    it 'should allow a teacher to se services' do 
+        teacher = Teacher.new(30, 'Physics', name='Unknown')
+        expect(teacher.can_use_services?).to be true
     end
 end
 end
